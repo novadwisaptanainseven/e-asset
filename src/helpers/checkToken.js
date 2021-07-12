42b31c6a506f951a52852a0f8c1cae8fe0c70d58
Nova Dwi Sapta Nain Seven <novadwisaptans@gmail.com>
2021-07-12 16:04:15 +0800

    Update

--- src/helpers/checkToken.js
+++ src/helpers/checkToken.js
@@ -1,17 +1,18 @@
-import Swal from "sweetalert2";
-import withReactContent from "sweetalert2-react-content";
+// import Swal from "sweetalert2";
+// import withReactContent from "sweetalert2-react-content";
 
-const MySwal = withReactContent(Swal);
+// const MySwal = withReactContent(Swal);
 
 export const checkToken = (history) => {
   if (localStorage.token) {
-    MySwal.fire({
-      icon: "error",
-      title: "Akses Dilarang",
-      text: "Anda harus logout terlebih dahulu!",
-      showConfirmButton: true,
-    }).then((result) => {
-      history.goBack();
-    });
+    // MySwal.fire({
+    //   icon: "error",
+    //   title: "Akses Dilarang",
+    //   text: "Anda harus logout terlebih dahulu!",
+    //   showConfirmButton: true,
+    // }).then((result) => {
+    //   history.goBack();
+    // });
+    history.goBack();
   }
 };
